f46b9fd2ec6f3d65b95daf4c3141ac1358fbfce1
swarksoft <swarksoft@gmail.com>
2015-07-13 22:35:44 +0200

    Modificado pie y apartado de creditos

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+/* GET home page. */
+router.get('/author', function(req, res) {
+  res.render('author', { nombre: 'Manuel Navarro' });
+});
+
 module.exports = router;
