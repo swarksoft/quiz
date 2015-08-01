317084dde2dc39e61c1975efc1b06a18f843a668
swarksoft <swarksoft@gmail.com>
2015-08-01 22:16:59 +0200

    Listado de preguntas

--- models/models.js
+++ models/models.js
@@ -33,11 +33,13 @@ exports.Quiz = Quiz; //exportar definción de tabla Quiz
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
-	//success(..) ejecuta el manejador una vez creada la tabla
+	//then(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count().then(function (count){
 		if (count === 0){ //la tabla se inicializa sólo si está vacía
 			Quiz.create({ pregunta: 'Capital de Italia', 
-						  respuesta: 'Roma'})
+						  respuesta: 'Roma'});
+			Quiz.create({ pregunta: 'Capital de Portugal', 
+						  respuesta: 'Lisboa'})
 			.then(function(){console.log('Base de datos inicializa')});
 		};
 	});
